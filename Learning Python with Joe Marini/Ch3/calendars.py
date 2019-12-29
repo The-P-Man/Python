7bc6143bded181c43409958e3685029b6ea5d17b
Philip Mannering <philmannering@gmail.com>
2019-12-29 22:51:57 +0000

    Files moved to the right folders
    Advent of Code files moved from Learning Python with Joe Marini

--- Learning Python with Joe Marini/Ch3/calendars.py
+++ Learning Python with Joe Marini/Ch3/calendars.py
@@ -4,43 +4,52 @@
 # import the calendar module
 import calendar
 
-
-
 # create a plain text calendar
 c = calendar.TextCalendar(calendar.MONDAY)
+print('The 2020 Calendar')
 c.pryear(2020)
-c.prmonth(2020, 1)
+print()
 
-# Print all dates for weeks that fall in the January 2020\
-for date in c.itermonthdates(2020, 1): print(date, end=' ')
+print('The January 2020 Calendar')
+c.prmonth(2020, 1)
+print()
+
+# Print all dates for weeks that fall in the January 2020
+print('All the dates in January 2020')
+for date in c.itermonthdates(2020, 1): 
+    print(date, end=' ')
+print('\n')
 
 # loop over the days of a month. zeroes mean that the day of the week is in an overlapping month
-for day in c.itermonthdays(2020, 1): print(day, end=' ')
-
+print('All the days in January 2020')
+for day in c.itermonthdays(2020, 1): 
+    print(day, end=' ')
+print('\n')
 
 # # create an HTML formatted calendar
 # hc = calendar.HTMLCalendar(0)
 # print(hc.formatmonth(2020, 1))
 
 # The Calendar module provides useful utilities for the given locale, such as the names of days and months in both full and abbreviated forms
-for month in calendar.month_name: print(month, end=' ')
-for day in calendar.day_name: print(day, end=' ')
+print('Print the months of the year')
+print(list(calendar.month_name))
+print('\n')
 
+print(list(calendar.day_name))
+print()
 # # List of months
 # print(list(calendar.month_name))
 
 
-# Calculate days based on a rule: For example, consider a team meeting on the first Friday of every month. To figure out what days that would be for each month, we can use this script:
-
-year = 2020
-month = 2
+# A team meet on the first Friday of every month. What day is that in January?
 
 # Get nested list of dates within weeks
-cal = calendar.monthcalendar(year, month)
+cal = calendar.monthcalendar(2020, calendar.January)
 # Can also use calendar.FRIDAY (== 4)
 date = cal[0][4] if cal[0][4] != 0 else cal[1][4]
-print('The first Friday is {} {} {}'.format(date, calendar.month_abbr[month], year))
-c.prmonth(year, month)
+print('The first Friday is', date)
+print('See for yourself')
+c.prmonth(2020, calendar.January)
 
 
 
--- Learning Python with Joe Marini/Ch3/dates.py
+++ Learning Python with Joe Marini/Ch3/dates.py
@@ -1,5 +1,5 @@
-# Working with Date Information
-# ========================================
+# Working with Date
+# =================
 
 from datetime import date, time, datetime
 
@@ -7,7 +7,7 @@ from datetime import date, time, datetime
 def main():
     # DATE OBJECTS
 
-    # get today's date from the date class
+    # Get today's date from the date class
     today = date.today()
     print('The date is', today)
     print('The date is', today.strftime('%A, %d %B %Y'))
@@ -16,25 +16,32 @@ def main():
     print('The year is', today.year)
     print('The month is', today.month)
     print('The day is', today.day)
-    print('The week is', today.isocalendar()[1])
     print('The weekday is', today.weekday())
+
+    # The isocalendar method gets the (year, week, dayofweek)
+    year, week, dayofweek = today.isocalendar()
+    print('The isocalendar year is', year)
+    print('The isocalendar week is', week)
+    print('The isocalendar weekday is', dayofweek)
     
     # Get today's weekday (0=monday, 6=sunday)
     days = ["Monday", "Tuesday", "Wednesday", "Thursday",\
             "Friday", "Saturday", "Sunday"]
 
     print('The weekday is', days[today.weekday()])
+    print()
 
-    # Get today's date from the datetime class
+    # Get today's datetime from the datetime class
     today = datetime.today()
     print('The datetime is', today)
-
-    # Get the time
-    time = datetime.time(datetime.today())
+    print()
+    # Get the current time
+    time = today.time()
     print('The time is', time)
     print('The hour is', time.hour)
     print('The minute is', time.minute)
     print('The second is', time.second)
+    print('The microsecond is', time.microsecond)
 
 
 if __name__ == '__main__':
